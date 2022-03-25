10f8430b693a577104a140f5cef3cb0e5226f758
AgungSyukur <agung.syukur@widyatama.ac.id>
2022-03-25 22:29:20 +0700

    Edit_Text

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     }
     setContentView(R.layout.activity_main);
     LinearLayout linearlayout_name = (LinearLayout)findViewById(R.id.LinearLayout);
-    TextView textview_name = new TextView(this);
-    textveiw_name.setText(“Hello I am Text View”);
-    linearLayout.addView(textview_name);
+    EditText edittext_name = new EditText(this);
+    edittext_name.setHint(“Username”);
+    linearLayout.addView(edittext_name);
 }
